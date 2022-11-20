7cf1ff0263e9924aebc5a02ce952890f30fa93bb
SungyoonRyu <rara0620@naver.com>
2022-11-20 03:58:12 -0800

    feat: add main page

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import { Landing, Login, Signup } from "../pages";
+import { Landing, Login, Main, Signup } from "../pages";
 
 
 export default function Router() {
@@ -9,6 +9,7 @@ export default function Router() {
             <Routes>
                 <Route path="/" element={<Landing />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/main" element={<Main />} />
                 <Route path="/signup" element={<Signup />} />
             </Routes>
         </BrowserRouter>
