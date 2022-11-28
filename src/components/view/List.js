cf39c5fe02c626ff5eb55c14f92018259f04a170
ross1573 <ross1573@naver.com>
2022-11-28 16:18:46 +0900

    fix: issue on array mapping

--- src/components/view/List.js
+++ src/components/view/List.js
@@ -1,21 +1,19 @@
 
 
 export default function ListView(props) {
-    const data = props.nodeData.nodes.map((node) => {
-        return (
-            <>
-                <p> Id: {node.id} </p>
-                <p> Title: {node.title} </p>
-                <p> Group: {node.group} </p>
-            </>
-        );
-    })
-
-    if (props.activate) {
-        return(
-            <>
-              {data}  
-            </>
-        );
-    }
+  if (props.activate) {
+    return(
+      <>
+        {props.nodeData.nodes.map((node) => {
+          return (
+            <p key={node.id}> 
+              Title: {node.title}
+              <br/>
+              Group: {node.group}
+            </p>
+          );
+        })}  
+      </>
+    );
+  }
 }
