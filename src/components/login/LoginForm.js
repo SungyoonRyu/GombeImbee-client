2ea7f182d771be46bbc08633125ea960633c7085
ross1573 <ross1573@naver.com>
2022-12-04 07:13:39 +0900

    feat: add login and signup

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,19 +1,59 @@
 import styled from "styled-components";
+import axios from "axios";
 
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import config from "../../utils/config.json";
 
 export default function LoginForm() {
+    const [id, setId] = useState();
+    const [pw, setPw] = useState();
+    const [msg, setMsg] = useState();
     const navigate = useNavigate();
+
+    const cancel = () => {
+        navigate("/");
+    }
+
+    const login = (ele) => {
+        ele.preventDefault();
+        if (!id) return alert("ID를 입력하세요.");          
+        else if (!pw) return alert("Password를 입력하세요.");
+
+        let loginData = {id: id, pw: pw};
+        axios.post(config.ip+config.port+'/usr/signin', loginData)
+            .then(res => {
+                if (res.status == 200) {
+                    alert("Login 성공!");
+                    navigate("/main");
+                }
+                else if (res.status == 400) setMsg("Id와 Password를 입력해주세요.");
+                else if (res.status == 401) setMsg("존재하지 않는 ID 입니다.");
+                else if (res.status == 402) setMsg("Password가 틀립니다.");
+                else setMsg("UNKNOWN_ERROR");
+            })
+    }
+
     return (
-        <StForm>
+        <StForm onSubmit={login}>
             <StLabel>User ID</StLabel>
-            <StInput placeholder="이메일 입력"/>
+            <StInput
+                placeholder="이메일 입력" 
+                onChange={(e)=>setId(e.target.value)}
+            />
 
             <StLabel>Password</StLabel>
-            <StInput placeholder="비밀번호 입력"/>
+            <StInput 
+                type="password" 
+                placeholder="비밀번호 입력" 
+                onChange={(e)=>setPw(e.target.value)}
+            />
 
             <div style={{margin:'60px auto', width:'400px'}}>
-                <StButton onClick={() => navigate("/")}>취소</StButton>
+                <StButton onClick={() => cancel()}>취소</StButton>
+            </div>
+            <div style={{margin:'60px auto', width:'400px'}}>
+                <StButton>로그인</StButton>
             </div>
         </StForm>
     );
--- src/components/signup/SignupForm.js
+++ src/components/signup/SignupForm.js
@@ -1,23 +1,54 @@
 import styled from "styled-components";
+import axios from "axios";
 
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import config from "../../utils/config.json";
 
 export default function SignupForm() {
+    const [name, setName] = useState();
+    const [id, setId] = useState();
+    const [pw, setPw] = useState();
+    const [pwVerify, setPwVerify] = useState();
+    const [email, setEmail] = useState();
+    const [msg, setMsg] = useState();
     const navigate = useNavigate();
-    return (
-        <StForm>
-            <StLabel>User name</StLabel>
-            <StInput />
-            <StLabel>User ID</StLabel>
-            <StInput />
-            <StLabel>Password</StLabel>
-            <StInput />
-            <StLabel>Password check</StLabel>
-            <StInput />
-            <StLabel>User Emain</StLabel>
-            <StInput />
 
-            <StButton onClick={() => navigate("/")}>Signup</StButton>
+    const signup = (ele) => {
+        ele.preventDefault();
+        if (!name) return alert("이름을 입력하세요.");
+        else if (!id) return alert("ID를 입력하세요.");          
+        else if (!pw) return alert("Password를 입력하세요.");
+        else if (!pwVerify) return alert("비밀번호 확인을 입력하세요.");
+        else if (!email) return alert("이메일을 입력하세요.");
+        else if (pw != pwVerify) return alert("비밀번호가 일치하지 않습니다.");
+
+        let signupData = {name: name, id: id, pw: pw, email: email};
+        axios.post(config.ip+config.port+'/usr/signup', signupData)
+            .then((res) => {
+                if (res.status == 200) {
+                    alert("가입성공!");
+                    navigate("/");
+                }
+                else if (res.status == 400) setMsg("모든 정보를 기입하여 주시기 바랍니다.");
+                else if (res.status == 401) setMsg("이미 존재하는 Id 입니다.");
+            });        
+    }
+
+    return (
+        <StForm onSubmit={signup}>
+            <StLabel>User name</StLabel>
+            <StInput onChange={(e)=>setName(e.target.value)}/>
+            <StLabel>User ID</StLabel>
+            <StInput onChange={(e)=>setId(e.target.value)}/>
+            <StLabel>Password</StLabel>
+            <StInput type="password" onChange={(e)=>setPw(e.target.value)}/>
+            <StLabel>Password check</StLabel>
+            <StInput type="password" onChange={(e)=>setPwVerify(e.target.value)}/>
+            <StLabel>User Emain</StLabel>
+            <StInput onChange={(e)=>setEmail(e.target.value)}/>
+
+            <StButton>Signup</StButton>
 
         </StForm>
     );
