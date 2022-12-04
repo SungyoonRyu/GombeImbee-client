bc7da3e740fb4ef8055ffef221ed6414dc9552c5
SungyoonRyu <rara0620@naver.com>
2022-12-04 03:01:27 -0800

    feat: add style to Workspace list

--- src/components/main/WorkspaceList.js
+++ src/components/main/WorkspaceList.js
@@ -1,7 +1,17 @@
+import styled from "styled-components";
+
 export default function WorkspaceList() {
     return (
-        <>
+        <StWrapper>
             list
-        </>
+        </StWrapper>
     );
 }
+
+const StWrapper = styled.div`
+    margin: 50px 0px;
+`;
+
+const StWSRow = styled.div`
+    font-size: 20px;
+`;
