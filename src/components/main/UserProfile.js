feba0d5d70bb7693175ca4387e13d652919d981e
SungyoonRyu <rara0620@naver.com>
2022-12-04 05:16:00 -0800

    feat: styled sidebar user profile

--- src/components/main/UserProfile.js
+++ src/components/main/UserProfile.js
@@ -1,7 +1,32 @@
+import styled from "styled-components";
+import { useRecoilValue } from "recoil";
+
+import { isLoginState } from "../../utils/atom";
+
 export default function UserPofile() {
+    const userInfo = useRecoilValue(isLoginState);
     return (
-        <>
+        <StWrapper>
             user profile
-        </>
+            <StUserImg>user.img</StUserImg>
+            <StUserName>{userInfo.id}</StUserName>
+        </StWrapper>
     );
 }
+
+const StWrapper = styled.div`
+    margin: 20px 0px;
+`;
+
+const StUserImg = styled.div`
+    margin: auto;
+    height: 120px;
+    width: 120px;
+    border: black solid 1px;
+`;
+
+const StUserName = styled.div`
+    margin: 10px auto;
+    width: 230px;
+    text-align: center;
+`;
